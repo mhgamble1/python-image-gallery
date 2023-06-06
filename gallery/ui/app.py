fef42219947ad8263b34bdf0f42b844524216d26
mhgamble1 <mhgamble1@gmail.com>
2023-06-06 01:15:34 +0000

    Update imports

--- gallery/ui/app.py
+++ gallery/ui/app.py
@@ -11,3 +11,15 @@ def admin():
 def delete(username):
     delete_user_query(username)
     return redirect(url_for('admin'))
+
+@app.route("/admin/addUser", methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        full_name = request.form.get('full_name')
+        add_user_query(username, password, full_name)
+        return redirect(url_for('admin'))
+
+    return render_template('add_user.html')
+
