af142137b9ae2028adf3cb558b8beb54fcfdf166
mhgamble1 <mhgamble1@gmail.com>
2023-06-13 00:30:37 +0000

    Update prod-1.1 script

--- ec2-scripts/ec2-prod-1.1.sh
+++ ec2-scripts/ec2-prod-1.1.sh
@@ -6,13 +6,13 @@ CONFIG_BUCKET="edu.au.cc.image-gallery-config-mg"
 
 # Install packages
 yum -y update
-yum install -y python3 git nginx postgresql15 postgresql-devel gcc python3-devel
+yum install -y python3 pip git nginx postgresql15 postgresql-devel gcc python3-devel
 
 # Configure/install custom software
 cd /home/ec2-user
 git clone https://github.com/mhgamble1/python-image-gallery.git
 chown -R ec2-user:ec2-user python-image-gallery
-su ec2-user -l -c "cd ~/python-image-gallery && pip3 install -r requirements.txt --user"
+su ec2-user -l -c "cd ~/python-image-gallery && pip install -r requirements.txt --user"
 
 aws s3 cp s3://${CONFIG_BUCKET}/nginx/nginx.conf /etc/nginx
 aws s3 cp s3://${CONFIG_BUCKET}/nginx/default.d/image_gallery.conf /etc/nginx/default.d
