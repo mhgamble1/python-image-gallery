7e2cf9be9259605e60f6eaefe9bb91771eeb97b9
Mark Gamble <mhgamble1@gmail.com>
2023-07-01 19:59:17 +0000

    Add auth

--- gallery/ui/app.py
+++ gallery/ui/app.py
@@ -1,11 +1,27 @@
-from flask import Flask, request, render_template, redirect, url_for
+import json
+from flask import Flask, request, render_template, redirect, url_for, session
 from ..tools.db import list_users_query, delete_user_query, add_user_query, edit_user_query
 
+from ..tools.user import User
+from ..tools.postgres_user_dao import PostgresUserDAO
+from ..tools.db import connect
+from ..tools.secrets import get_secret_flask_session
+
 app = Flask(__name__)
+secret = get_secret_flask_session()
+secret_dict = json.loads(secret)
+app.secret_key = secret_dict["secret_key"]
+
+connect()
+
+def get_user_dao():
+    return PostgresUserDAO()
 
 # admin
 @app.route("/admin", methods=['GET', 'POST'])
 def admin():
+    if not check_admin():
+        return redirect('/login')
     return render_template('admin.html', users=list_users_query()) 
 
 @app.route("/admin/delete/<username>", methods=['POST'])
@@ -39,9 +55,24 @@ def edit(username):
 def index():
     return render_template('index.html')
 
+@app.route("/invalidLogin")
+def invalidLogin():
+    return "Invalid"
+
+def check_admin():
+    return 'username' in session and session['username'] == 'test'
+
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        user = get_user_dao().get_user_by_username(request.form["username"])
+        if user is None or user.password != request.form["password"]:
+            return redirect('/invalidLogin')
+        else:
+            session['username'] = request.form["username"]
+            return redirect("/debugSession")
+    else:
+        return render_template('login.html')
 
 @app.route("/upload", methods=['GET', 'POST'])
 def upload():
@@ -50,3 +81,12 @@ def upload():
 @app.route("/images", methods=['GET', 'POST'])
 def images():
     return render_template('images.html')
+
+# sessions
+@app.route("/debugSession")
+def debugSession():
+    result = ""
+    for key,value in session.items():
+        result += key+"->"+str(value)+"<br />"
+    return result
+
